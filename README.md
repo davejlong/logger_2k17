34fd1062134c7d3800880fa699b391a709492408
Dave Long <dlong@cagedata.com>
2017-03-09 22:28:37 -0500

    Adds documentation

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Logger2k17
 
-**TODO: Add description**
+Finally, a Logger that understands the lingo of modern times. No more of those meaningless log methods like `info` and `warn`. Now just use common terms like `fyi` and `wtf`. Want to log a message that's just for the engineers to see what's going on? Don't `debug` it. `wharrgarbl` it!
 
 ## Installation
 
@@ -16,4 +16,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/logger_2k17](https://hexdocs.pm/logger_2k17).
-
--- lib/logger_2k17.ex
+++ lib/logger_2k17.ex
@@ -1,22 +1,34 @@
 defmodule Logger2k17 do
-  import Logger
-
   @moduledoc """
-  Documentation for Logger2k17.
+  Finally a logger for 2k17!
   """
 
+  import Logger
+
+  @doc """
+  Logs a message used when seeing what's happening under the hood
+  """
   def wharrgarbl(chardata_or_fun, metadata \\ []) do
     debug(chardata_or_fun, metadata)
   end
 
+  @doc """
+  Logs a message FYI
+  """
   def fyi(chardata_or_fun, metadata \\ []) do
     info(chardata_or_fun, metadata)
   end
 
+  @doc """
+  Things aren't dead yet, but WTF is going on here?
+  """
   def wtf(chardata_or_fun, metadata \\ []) do
     warn(chardata_or_fun, metadata)
   end
 
+  @doc """
+  OMG, shit has hit the fan and we're going down hard.
+  """
   def omg(chardata_or_fun, metadata \\ []) do
     error(chardata_or_fun, metadata)
   end
