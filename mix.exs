923e9b673dc201b61f287590ac97452a4c17d014
Dave Long <dlong@cagedata.com>
2017-03-09 22:34:10 -0500

    Add package info and license

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Logger2k17.Mixfile do
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   # Configuration for the OTP application
@@ -18,16 +20,24 @@ defmodule Logger2k17.Mixfile do
     [extra_applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:my_dep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    Finally a logger module for 2k17. Replaces Logger with a new and fresh
+    module with meaningful methods like `fyi` and `omg` instead of `info` and
+    `error`.
+    """
+  end
+
+  defp package do
+    [name: :logger_2k17,
+     files: ["lib", "mix.exs", "*.md", "LICENSE.txt"],
+     maintainers: ["Dave Long"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/davejlong/logger_2k17"}
+    ]
   end
 end
